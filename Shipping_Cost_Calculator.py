7b4d67e2501a21d46e960d337d4d731b616af268
Selaman Apps <111949719+selamanapps@users.noreply.github.com>
2024-08-17 03:24:17 +0300

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,5 @@
- # Here is a new update by <your GitHub username>
+# Here is another update by selamanapps
+# Here is a new update by <your GitHub username>
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
